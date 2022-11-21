665bfc070b0f550eb05b79010844df71ff1a3256
sammy2342 <sammyfares303@gmail.com>
2022-11-21 14:18:54 -0500

    complete ticker

---
+++ src/components/CoinHeader/CoinHeader.jsx
@@ -0,0 +1,43 @@
+import AliceCarousel from 'react-alice-carousel'
+import 'react-alice-carousel/lib/alice-carousel.css'
+import { useEffect, useState } from 'react'
+import './CoinHeader.css'
+
+export default function CoinHeader({coins, setCoins}) {
+
+    const items = coins.map((coin) => {
+        return <div className='coin-ticker-container'>
+                <div className='image-coin-ticker'><img src={coin.image} alt="" id='coin-ticker-image' /></div>
+                <div className='coin-name-container'>
+                    <div className='name-coin-ticker'>{coin.name}</div>
+                    <div className='symbol-coin-ticker'>{coin.symbol}</div>
+                </div>
+            </div>
+    })
+
+    const responsive = {
+        0: {
+            items: 1,
+        },
+        1024: {
+            items: 3
+        }
+    }
+
+
+    return (
+        <div className='coin-header-page'>
+                <h1 className='coin-title-home-page'>Cortfolio tracker</h1>
+                <h4 className='coin-home-page'>Get All The Info For Your Favorite Crypto Currency</h4>
+                <AliceCarousel 
+                    // mouseTracking this if you want to stop it when you put your mouse on it
+                    infinite
+                    items={items}
+                    autoPlayInterval={500}
+                    animationDuration={1500}
+                    autoPlay
+                    responsive={responsive}
+                />
+        </div>
+    )
+}
--- src/pages/AllCoinPage/AllCoinPage.jsx
+++ src/pages/AllCoinPage/AllCoinPage.jsx
@@ -1,6 +1,7 @@
 import * as coinsAPI from '../../utilities/coins-api'
 import { useEffect, useState } from 'react'
 import './AllCoinPage.css'
+import CoinHeader from '../../components/CoinHeader/CoinHeader'
 
 export default function AllCoinPage() {
     const [coins, setCoins] = useState([])
@@ -9,7 +10,7 @@ export default function AllCoinPage() {
         console.log('this effect is running')
         async function getCoin() {
             const coin = await coinsAPI.getAllCoin()
-            console.log(coin)
+            // console.log(coin)
             setCoins(coin)
             // const data = res. - axios
             // const data = await res.json() - fetch
@@ -20,8 +21,10 @@ export default function AllCoinPage() {
 
     return (
         <>
-            <h1>AllCoinPage</h1>
+            <CoinHeader coins={coins} setCoins={setCoins}/>
             <div className='coinContainer'>
+                <h1 className='search-coin-title'>Cryptocurrency Find Your Coin</h1>
+                <input type="search" name="" id="coin-page-search-input-crypto" placeholder='Search For Crypto Coin...'/>
                 <table className='table-coin'>
                     <tr className='table-row-coin'>
                         <td>Logo</td>
